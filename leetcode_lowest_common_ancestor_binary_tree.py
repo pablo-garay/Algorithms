7b2e026b5bfcf4d5ed48a643ff7843408f2afbcb
Pablo Garay <pgaray@roshka.com>
2019-10-20 19:42:19 -0700

    Added solution to leetcode problem

---
+++ leetcode_lowest_common_ancestor_binary_tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):  # O(n) where n: number of tree nodes
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+
+        ret1, ret2 = self.lowestCommonAncestor(root.left, p, q), self.lowestCommonAncestor(root.right, p, q)
+
+        if ret1 is not None and ret2 is not None:
+            return root
+
+        if (root.val == p.val or root.val == q.val) and (ret1 is not None or ret2 is not None):  # case: node descendant of itself
+            return root
+
+        if ret1 is not None:
+            return ret1
+
+        if ret2 is not None:
+            return ret2
+
+        if root.val == p.val or root.val == q.val:
+            return root
+
+        return None
+
+
+print Solution().lowestCommonAncestor(root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1)
