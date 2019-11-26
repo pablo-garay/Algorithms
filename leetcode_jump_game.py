25c83a14c5dc6e85a70f2dc6b4269877ef7faab4
Pablo Garay <pgaray@roshka.com>
2019-11-26 00:02:01 -0800

    Added solution to leetcode problem

---
+++ leetcode_jump_game.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def canJump(self, nums):
+        goal = len(nums) - 1
+
+        for i in reversed(xrange(len(nums) - 1)):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return (goal == 0)
+
+
+print Solution().canJump([2,3,1,1,4])
+print Solution().canJump([3,2,1,0,4])
+# print Solution().canJump([])
+print Solution().canJump([0])
+print Solution().canJump([0, 0])
+print Solution().canJump([0, 1])
+print Solution().canJump([1, 0])
