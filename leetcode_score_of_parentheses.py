483ae0b287bafc3cdd4c6c6dfba0c0842dfe56bd
Pablo Garay <pgaray@roshka.com>
2020-01-12 20:10:14 -0800

    Added solution to leetcode problem

---
+++ leetcode_score_of_parentheses.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    curr = 0
+
+    def scoreOfParentheses(self, S):
+        curr_sum = 0
+
+        while self.curr < len(S):
+            if S[self.curr] == ")":
+                if S[self.curr - 1] == "(":
+                    curr_sum += 1
+                    return curr_sum
+
+                else:
+                    curr_sum *= 2
+                    return curr_sum
+
+            if S[self.curr] == "(":
+                self.curr += 1
+                curr_sum += self.scoreOfParentheses(S)
+
+            self.curr += 1
+
+        return curr_sum
+
+
+print Solution().scoreOfParentheses("()")  # 1
+print Solution().scoreOfParentheses("(())")  # 2
+print Solution().scoreOfParentheses("()()")  # 2
+print Solution().scoreOfParentheses("((())())")  # 6
+print Solution().scoreOfParentheses("(()(()))")  # 6
+print Solution().scoreOfParentheses("()(())")  # 3
+print Solution().scoreOfParentheses("((())())((())())((())())")  # 18
