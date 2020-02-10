6c1ab53943c40c0d075f272cdcd2d9ad8951174c
Pablo Garay <pgaray@roshka.com>
2020-02-10 00:10:57 -0800

    Added solution to leetcode problem

---
+++ leetcode_divisor_game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def divisorGame(self, N):
+        return N % 2 == 0
+
+# print Solution().divisorGame(5)
+print Solution().divisorGame(1000)
+print Solution().divisorGame(1)
+print Solution().divisorGame(2)
+print Solution().divisorGame(3)
+
+
