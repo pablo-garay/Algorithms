dd27ae045d1b6a9f8eb4edc67befa4f3e62a24de
Pablo Garay <pgaray@roshka.com>
2020-02-17 21:47:16 -0800

    Added solution to leetcode problem

--- leetcode_flower_planting_with_no_adjacent.py
+++ leetcode_flower_planting_with_no_adjacent.py
@@ -1,23 +1,19 @@
-from collections import defaultdict
-
 class Solution(object):  # O(m + n). Optimal as we need to check each node and edge in the worst case
     def gardenNoAdj(self, N, paths):
         options = [set([1, 2, 3, 4]) for _ in xrange(N)]
         color = [None for _ in xrange(N)]
-        adj = defaultdict(list)
+        adj = [[] for _ in xrange(N)]
 
         for (a, b) in paths:
-            a, b = (a - 1, b - 1)
-            adj[a].append(b)
-            adj[b].append(a)
+            adj[a - 1].append(b - 1)
+            adj[b - 1].append(a - 1)
 
         for node in xrange(N):
-            if not color[node]:
-                color[node] = options[node].pop()
+            color[node] = options[node].pop()
 
-                for neigh in adj[node]:
-                    if not color[neigh]:
-                        options[neigh].discard(color[node])
+            for neigh in adj[node]:
+                if not color[neigh]:
+                    options[neigh].discard(color[node])
 
         return color
 
