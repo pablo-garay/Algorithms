cae237aed5edd89e66244252e9696183a5213385
Pablo Garay <pagarayf@microsoft.com>
2021-07-18 01:01:16 -0700

    Added solution to leetcode problem

---
+++ leetcode_remove_nth_node_from_end_of_list.py
@@ -0,0 +1,26 @@
+class Solution(object):  # O(n) as each node in linked list is traversed once only (optimal as need to visit each once at least)
+    count = 0
+
+    def removeNthFromEnd(self, head, n):
+        return self.checkNode(head, n)
+
+    def checkNode(self, node, n):
+        if node is None:
+            return None
+
+        next_node = self.checkNode(node.next, n)
+
+        if node.next is None:
+            self.count = 1
+        else:
+            self.count += 1
+
+        if self.count == n:
+            return node.next
+
+        node.next = next_node
+
+        return node
+
+
+print Solution().removeNthFromEnd(head=[1, 2, 3, 4, 5], n=2)
