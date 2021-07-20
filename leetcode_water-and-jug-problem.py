0174fa517f59e662c39f7c4537c924dbe72ceab3
Pablo Garay <pagarayf@microsoft.com>
2021-07-19 19:58:26 -0700

    Added solution to leetcode problem

--- leetcode_water-and-jug-problem.py
+++ leetcode_water-and-jug-problem.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution(object):  # BFS solution: O(|nodes| + |edges|). Nodes: states, edges: possible actions
     def canMeasureWater(self, jug1Capacity, jug2Capacity, targetCapacity):
         if targetCapacity > jug1Capacity + jug2Capacity:
             return False
@@ -8,9 +8,7 @@ class Solution(object):
 
     def bfs(self, jcap1, jcap2, targetCapacity):
         parent = {(0, 0): None}
-        level = {}
         frontier = [(0, 0)]
-        curr_level = 1
 
         while frontier:
             next_frontier = []
@@ -19,43 +17,13 @@ class Solution(object):
                 if j1 + j2 == targetCapacity:
                     return True
 
-                if j1 < jcap1 and (jcap1, j2) not in parent:
-                    next_frontier.append((jcap1, j2))
-                    parent[(jcap1, j2)] = (j1, j2)
+                for state in [(jcap1, j2), (j1, jcap2), (0, j2), (j1, 0),
+                              (0, j1 + j2) if j1 + j2 <= jcap2 else (j1 - (jcap2 - j2), jcap2),
+                              (j1 + j2, 0) if j1 + j2 <= jcap1 else (jcap1, j2 - (jcap1 - j1))]:
 
-                if j2 < jcap2 and (j1, jcap2) not in parent:
-                    next_frontier.append((j1, jcap2))
-                    parent[(j1, jcap2)] = (j1, j2)
-
-                if (0, j2) not in parent:
-                    next_frontier.append((0, j2))
-                    parent[(0, j2)] = (j1, j2)
-
-                if (j1, 0) not in parent:
-                    next_frontier.append((j1, 0))
-                    parent[(j1, 0)] = (j1, j2)
-
-                if j2 < jcap2 and j1 > 0:
-                    if j2 + j1 <= jcap2:
-                        if (0, j1 + j2) not in parent:
-                            next_frontier.append((0, j1 + j2))
-                            parent[(0, j1 + j2)] = (j1, j2)
-                    else:
-                        diff = jcap2 - j2
-                        if (j1 - diff, jcap2) not in parent:
-                            next_frontier.append((j1 - diff, jcap2))
-                            parent[(j1 - diff, jcap2)] = (j1, j2)
-
-                if j1 < jcap1 and j2 > 0:
-                    if j1 + j2 <= jcap1:
-                        if (j1 + j2, 0) not in parent:
-                            next_frontier.append((j1 + j2, 0))
-                            parent[(j1 + j2, 0)] = (j1, j2)
-                    else:
-                        diff = jcap1 - j1
-                        if (jcap1, j2 - diff) not in parent:
-                            next_frontier.append((jcap1, j2 - diff))
-                            parent[(jcap1, j2 - diff)] = (j1, j2)
+                    if state not in parent:
+                        next_frontier.append(state)
+                        parent[state] = (j1, j2)
 
             frontier = next_frontier
 
