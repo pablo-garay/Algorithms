b2be45ca499459ef1df59052b692e6f17259c192
Pablo Garay <pagarayf@microsoft.com>
2021-08-16 00:08:40 -0700

    Added solution to leetcode problem

---
+++ leetcode_sum-of-two-integers.c
@@ -0,0 +1,13 @@
+int getSum(int a, int b){
+        unsigned int sum_excluding_carries = a ^ b;
+        unsigned int carry = (a & b & 0xffffffff) << 1;
+        unsigned int new_carry;
+    
+        while(carry != 0){
+            new_carry = sum_excluding_carries & carry;
+            sum_excluding_carries = sum_excluding_carries ^ carry;
+            carry = (new_carry & 0xffffffff) << 1;
+        }
+
+        return sum_excluding_carries;
+}
