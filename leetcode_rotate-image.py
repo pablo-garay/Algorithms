d823c05806653f1f5038555ad85117f6354e71f9
Pablo Garay <pagarayf@microsoft.com>
2021-08-15 20:28:09 -0700

    Added solution to leetcode problem

--- leetcode_rotate-image.py
+++ leetcode_rotate-image.py
@@ -1,44 +1,21 @@
 class Solution(object):
     def rotate(self, matrix):
-        rows, cols = (len(matrix), len(matrix[0]))
-        last_row, last_col = rows - 1, cols - 1
+        def rotate_pos_clockwise(row, col):
+            return col, self.max_ind - row
 
-        offset = 0
-        cell_moves = cols - 1
+        n = len(matrix)
+        self.max_ind = n - 1
 
-        for row in range(rows / 2):
-            max_ind = last_row - offset
-            min_row = min_col = offset
-
-            for col in range(offset, cols - offset - 1):
-                next_row, next_col = (row, col)
-
-                curr_row, curr_col = (next_row, next_col)
-                delta = cell_moves - (max_ind - curr_col)
-                next_row, next_col = (min_row + delta, max_ind)
-                curr_val, next_val = (matrix[curr_row][curr_col], matrix[next_row][next_col])
-                matrix[next_row][next_col] = curr_val
-
-                curr_row, curr_col = (next_row, next_col)
-                delta = cell_moves - (max_ind - curr_row)
-                next_row, next_col = (max_ind, max_ind - delta)
-                curr_val, next_val = (next_val, matrix[next_row][next_col])
-                matrix[next_row][next_col] = curr_val
-
-                curr_row, curr_col = (next_row, next_col)
-                delta = cell_moves - (curr_col - min_col)
-                next_row, next_col = (max_ind - delta, min_col)
-                curr_val, next_val = (next_val, matrix[next_row][next_col])
-                matrix[next_row][next_col] = curr_val
-
-                curr_row, curr_col = (next_row, next_col)
-                delta = cell_moves - (curr_row - min_row)
-                next_row, next_col = (min_row, min_col + delta)
-                curr_val, next_val = (next_val, matrix[next_row][next_col])
-                matrix[next_row][next_col] = curr_val
-
-            offset += 1
-            cell_moves -= 2
+        for row in range(n / 2):
+            for col in range(row, n - row - 1):
+                up_val = matrix[row][col]
+                row_right, col_right = rotate_pos_clockwise(row, col)
+                row_bottom, col_bottom = rotate_pos_clockwise(row_right, col_right)
+                row_left, col_left = rotate_pos_clockwise(row_bottom, col_bottom)
+                matrix[row][col] = matrix[row_left][col_left]
+                matrix[row_left][col_left] = matrix[row_bottom][col_bottom]
+                matrix[row_bottom][col_bottom] = matrix[row_right][col_right]
+                matrix[row_right][col_right] = up_val
 
         print matrix
 
