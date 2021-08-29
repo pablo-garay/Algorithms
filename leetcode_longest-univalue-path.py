585c590fe4ecb88892db0447ef11cc66de1d621f
Pablo Garay <pagarayf@microsoft.com>
2021-08-28 20:28:45 -0700

    Added solution to leetcode problem

---
+++ leetcode_longest-univalue-path.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def longestUnivaluePath(self, root):
+        self.longest = 0
+        self.dfs(root)
+
+        return max(self.longest - 1, 0)
+
+    def dfs(self, curr):
+        if curr is None:
+            return 0
+
+        count_left = self.dfs(curr.left)
+        count_right = self.dfs(curr.right)
+
+        if count_left == 0 and count_right == 0:
+            curr_count = 1
+
+        elif count_left == 0:
+            if curr.val == curr.right.val:
+                curr_count = count_right + 1
+            else:
+                curr_count = 1
+
+        elif count_right == 0:
+            if curr.val == curr.left.val:
+                curr_count = count_left + 1
+            else:
+                curr_count = 1
+
+        else:
+            if curr.val != curr.left.val and curr.val != curr.right.val:
+                curr_count = 1
+            elif curr.val == curr.right.val == curr.left.val:
+                if self.longest < count_left + count_right + 1:
+                    self.longest = count_left + count_right + 1
+                curr_count = max(count_left + 1, count_right + 1)
+
+            elif curr.val == curr.right.val:
+                curr_count = count_right + 1
+            elif curr.val == curr.left.val:
+                curr_count = count_left + 1
+
+        if curr_count > self.longest:
+            self.longest = curr_count
+
+        return curr_count
