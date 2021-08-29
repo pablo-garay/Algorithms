a20622d022b791b2cbd364e607efc55c856282a7
Pablo Garay <pagarayf@microsoft.com>
2021-08-28 20:32:26 -0700

    Added solution to leetcode problem

--- leetcode_longest-univalue-path.py
+++ leetcode_longest-univalue-path.py
@@ -3,7 +3,7 @@ class Solution(object):  # One pass. Optimal as need to traverse whole tree in t
         self.longest = 0
         self.dfs(root)
 
-        return max(self.longest - 1, 0)
+        return self.longest
 
     def dfs(self, curr):
         if curr is None:
@@ -12,35 +12,8 @@ class Solution(object):  # One pass. Optimal as need to traverse whole tree in t
         count_left = self.dfs(curr.left)
         count_right = self.dfs(curr.right)
 
-        if count_left == 0 and count_right == 0:
-            curr_count = 1
+        left_side = count_left + 1 if curr.left and curr.val == curr.left.val else 0
+        right_side = count_right + 1 if curr.right and curr.val == curr.right.val else 0
 
-        elif count_left == 0:
-            if curr.val == curr.right.val:
-                curr_count = count_right + 1
-            else:
-                curr_count = 1
-
-        elif count_right == 0:
-            if curr.val == curr.left.val:
-                curr_count = count_left + 1
-            else:
-                curr_count = 1
-
-        else:
-            if curr.val != curr.left.val and curr.val != curr.right.val:
-                curr_count = 1
-            elif curr.val == curr.right.val == curr.left.val:
-                if self.longest < count_left + count_right + 1:
-                    self.longest = count_left + count_right + 1
-                curr_count = max(count_left + 1, count_right + 1)
-
-            elif curr.val == curr.right.val:
-                curr_count = count_right + 1
-            elif curr.val == curr.left.val:
-                curr_count = count_left + 1
-
-        if curr_count > self.longest:
-            self.longest = curr_count
-
-        return curr_count
+        self.longest = max(self.longest, left_side + right_side)
+        return max(left_side, right_side)
