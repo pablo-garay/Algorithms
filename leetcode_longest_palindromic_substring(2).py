4786026241a4b3f55cd5bf17d700322b962c02cb
Pablo Garay <pagarayf@microsoft.com>
2021-10-25 15:46:43 -0700

    Added solution to leetcode problem

---
+++ leetcode_longest_palindromic_substring(2).py
@@ -0,0 +1,24 @@
+class Solution(object):  # Borrowed solution. O(n^2)
+    def longestPalindrome(self, s):
+        res = ""
+        for i in xrange(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i + 1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+
+    # get the longest palindrome. Left, right are the middle indexes. From inner to outer
+    def helper(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left, right = (left - 1, right + 1)
+        return s[left + 1:right]  # there was one extra iteration (exclude left and right)
+
+
+print Solution().longestPalindrome("babad")
+print Solution().longestPalindrome("cbbd")
+print Solution().longestPalindrome("a")
