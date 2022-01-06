c243988232a13c09ec43cfb661ff00cda570f4c3
Pablo Garay <pagarayf@microsoft.com>
2022-01-05 19:47:00 -0800

    Added solution to leetcode problem

---
+++ leetcode_longest-common-subsequence.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):  # Time: O(mxn). Space: O(mxn)
+        self.text1, self.text2 = text1, text2
+        self.memo = [[0 for _ in xrange(len(text2) + 1)] for _ in xrange(len(text1) + 1)]
+
+        for i2 in reversed(xrange(len(text2))):
+            for i1 in reversed(xrange(len(text1))):
+                self.memo[i1][i2] = max(1 + self.memo[i1 + 1][i2 + 1] if self.text1[i1] == self.text2[i2] else 0,
+                                    self.memo[i1 + 1][i2],
+                                    self.memo[i1][i2 + 1])
+
+        return self.memo[0][0]
+
+
+print Solution().longestCommonSubsequence(text1 = "abcde", text2 = "ace" )
+print Solution().longestCommonSubsequence(text1 = "abc", text2 = "abc")
+print Solution().longestCommonSubsequence(text1 = "abc", text2 = "def")
+print Solution().longestCommonSubsequence(text1 = "abcd", text2 = "bcabcd")
+print Solution().longestCommonSubsequence(text1 = "abcd", text2 = "abcdefgh")
+print Solution().longestCommonSubsequence("hofubmnylkra", "pqhgxgdofcvmr")
+print Solution().longestCommonSubsequence("aaa", "aaaaa")
+print Solution().longestCommonSubsequence("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+print Solution().longestCommonSubsequence("mhunuzqrkzsnidwbun", "szulspmhwpazoxijwbq")
+print Solution().longestCommonSubsequence("papmretkborsrurgtina", "nsnupotstmnkfcfavaxgl")
