07ec9e2ca82f9ac233534760d7cc311c0e5509a8
Pablo Garay <pagarayf@microsoft.com>
2022-01-15 22:21:38 -0800

    Added solution to leetcode problem

--- leetcode_accounts-merge.py
+++ leetcode_accounts-merge.py
@@ -45,8 +45,3 @@ class Solution(object):  # Runtime: 180 ms, faster than 93.72%. Memory Usage: le
 
 print Solution().accountsMerge(accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]])
 print Solution().accountsMerge([["David","David0@m.co","David1@m.co"],["David","David3@m.co","David4@m.co"],["David","David4@m.co","David5@m.co"],["David","David2@m.co","David3@m.co"],["David","David1@m.co","David2@m.co"]])
-# print Solution().accountsMerge()
-
-
-
-
