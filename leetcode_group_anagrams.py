3cc2789623f5169fe46f1d1cdd65e319516b2a99
Pablo Garay <pagarayf@microsoft.com>
2022-01-17 17:15:47 -0800

    Added solution to leetcode problem

--- leetcode_group_anagrams.py
+++ leetcode_group_anagrams.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution(object):  # Runtime: 88 ms, faster than 76.17%
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
@@ -6,12 +6,12 @@ class Solution(object):
         """
         dict = {}
 
-        for s in strs:
-            sorted_s = "".join(sorted(s))
-            if sorted_s in dict:
+        for s in strs:  # Total: O(|strs|) * O(|s| log |s|)
+            sorted_s = "".join(sorted(s))  # O(|s| log |s|)
+            if sorted_s in dict:  # O(1)
                 dict[sorted_s].append(s)
             else:
-                dict[sorted_s] = [s]
+                dict[sorted_s] = [s]  # O(1)
 
         return dict.values()
 
