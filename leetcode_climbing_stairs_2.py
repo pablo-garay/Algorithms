c89147b1f5e7fc5d3dac6e1849ff97db63c7e43e
Pablo Garay <pagarayf@microsoft.com>
2022-01-25 00:18:16 -0800

    Added solution to leetcode problem

--- leetcode_climbing_stairs_2.py
+++ leetcode_climbing_stairs_2.py
@@ -1,13 +1,13 @@
-# FIBONACCI O(n)
+# FIBONACCI O(1)
 class Solution(object):
     def climbStairs(self, n):
         memo = [None for i in xrange(n + 1)]  # adding an extra element to array for convenience
-        memo[0] = memo[1] = 1  # for convenience i.e. this makes computations right
+        memo[n] = 1  # for convenience i.e. this makes computations right
+        memo[n - 1] = 1
 
-        for i in xrange(2, n + 1):
-            memo[i] = memo[i - 1] + memo[i - 2]
-        return memo[n]
-
+        for i in reversed(xrange(n - 1)):
+            memo[i] = memo[i + 1] + memo[i + 2]
+        return memo[0]
 
 print Solution().climbStairs(2)
 print Solution().climbStairs(3)
