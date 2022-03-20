b7c6ff85cb247b0cc545755d95ac633683612f0c
Pablo Garay <pagarayf@microsoft.com>
2022-03-19 18:11:01 -0700

    Added solution to leetcode problem

--- leetcode_populating-next-right-pointers-in-each-node-ii.py
+++ leetcode_populating-next-right-pointers-in-each-node-ii.py
@@ -1,15 +1,5 @@
-"""
-# Definition for a Node.
-class Node(object):
-    def __init__(self, val=0, left=None, right=None, next=None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
 class Solution(object):  # BFS: Time complexity is linear. Optimal as need to visit each node once in worst case.
-    def connect(self, root):
+    def connect(self, root):  # Runtime: 32 ms, faster than 98.01%
         if root == None: return
 
         frontier = [root]
@@ -31,14 +21,3 @@ class Solution(object):  # BFS: Time complexity is linear. Optimal as need to vi
             frontier = next_frontier
 
         return root
-
-    ## O(1) space solution - linear in time
-    # def connect(self, root):
-    #     while root and root.left:
-    #         next = root.left
-    #         while root:
-    #             root.left.next = root.right
-    #             root.right.next = root.next and root.next.left
-    #             root = root.next
-    #         root = next
-
