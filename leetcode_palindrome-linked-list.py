72566cd4daebdb7c3ac657bae41b7c2edea9a4bc
palenq <palenq@gmail.com>
2023-02-06 15:07:08 -0800

    Update leetcode_palindrome-linked-list.py

--- leetcode_palindrome-linked-list.py
+++ leetcode_palindrome-linked-list.py
@@ -1,21 +1,23 @@
-class Solution(object): # Runtime 20 ms Beats 97% Memory Beats 97%
-    def oddEvenList(self, head): # Time: O(n) Space: O(1) - Optimal as need to traverse whole linked list in worst case (also: one pass only)
-        if head is None or head.next is None: return head
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        self.ret = True
+        self.f(head, head)
+        return self.ret
 
-        first_link = head.next  # this is where the circling back should occur
-        odd = False  # track odd or even num of nodes
-        node = head
+    def f(self, left, right):
+        if right.next is not None:
+            left = self.f(left, right.next)
 
-        while node.next.next is not None:  # O(n)
-            visit_next = node.next
-            node.next = node.next.next
-            node = visit_next
-            odd = not odd
-        
-        if odd:
-            node.next.next = first_link
-            node.next = None
-        else: # even 
-            node.next = first_link
-        
-        return head
+        if left is None or left == right or right.next == left:
+            return None
+
+        if left.val != right.val:
+            self.ret = False
+            return None
+
+        return left.next
